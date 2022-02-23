0a11923c7e112d58d382732dbb1cc29ccf592efa
Crystal Fong <crystal.fong@harness.io>
2022-02-23 11:14:29 +0000

    feat: banner logic extracted into utils function

---
+++ src/modules/75-cf/utils/UsageLimitUtils.ts
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import type { ReactNode } from 'react'
+import { BannerType } from '@common/layouts/Constants'
+import type { CheckFeatureReturn } from 'framework/featureStore/featureStoreUtil'
+// import { formatToCompactNumber } from '@cf/utils/CFUtils'
+
+export const getBannerText = (
+  monthlyActiveUsers: CheckFeatureReturn,
+  additionalLicenseProps: Record<string, boolean>
+): ReactNode => {
+  const {
+    //   isEnterpriseEdition,
+    isFreeEdition
+    // isTeamEdition
+  } = additionalLicenseProps
+
+  const clientMauUsageCount = Number(monthlyActiveUsers?.featureDetail?.count)
+  const clientMauPlanLimit = Number(monthlyActiveUsers?.featureDetail?.limit)
+
+  const clientMauUsagePercentage = Math.trunc((clientMauUsageCount / clientMauPlanLimit) * 100)
+  //   const clientMauPlanLimitFormatted = formatToCompactNumber(clientMauPlanLimit)
+
+  const showInfoBanner = clientMauPlanLimit > 0 && clientMauUsagePercentage >= 90 && clientMauUsagePercentage < 100
+  //   const showWarningBanner = clientMauPlanLimit > 0 && clientMauUsagePercentage >= 100
+
+  let message!: string
+  let bannerType!: BannerType
+
+  if (isFreeEdition) {
+    if (showInfoBanner) {
+      ;(message = 'show info banner'),
+        //     getString('cf.planEnforcement.freePlan.approachingLimit', {
+        //     clientMauUsagePercentage,
+        //     clientMauPlanLimitFormatted
+        //   })),
+        (bannerType = BannerType.INFO)
+    }
+
+    // if (showWarningBanner) {
+    //   return {
+    //     message: () =>
+    //       getString('cf.planEnforcement.freePlan.upgradeRequired', {
+    //         clientMauPlanLimitFormatted
+    //       }),
+    //     bannerType: BannerType.LEVEL_UP
+    //   }
+    // }
+  }
+
+  //   if (isTeamEdition || isEnterpriseEdition) {
+  //     if (showInfoBanner) {
+  //       return {
+  //         message: () =>
+  //           getString('cf.planEnforcement.teamEnterprisePlan.approachingLimit', {
+  //             clientMauUsagePercentage
+  //           }),
+  //         bannerType: BannerType.INFO
+  //       }
+  //     }
+
+  //     if (showWarningBanner) {
+  //       return {
+  //         message: () => getString('cf.planEnforcement.teamEnterprisePlan.upgradeRequired'),
+  //         bannerType: BannerType.LEVEL_UP
+  //       }
+  //     }
+  //   }
+
+  console.log('in usage limit utils message, bannerType', message, bannerType)
+
+  return {
+    message,
+    bannerType
+  }
+}
